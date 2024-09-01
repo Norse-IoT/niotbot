ca3aab89fe1b9f45615ea852a708afc4eaa16a05
Zack Sargent <zack@zack.fyi>
2024-09-01 15:01:24 -0400

    limit to specific channels

--- niotbot.py
+++ niotbot.py
@@ -1,10 +1,21 @@
 import discord
 
 class NIoTBot(discord.Client):
+    """ Norse IoT Bot; currently handles social media"""
+
+    # this needs to be manually updated if you want to add more channels
+    ALLOWED_CHANNELS = [
+        1279865902241808535
+    ]
+
     async def on_ready(self):
         print('Logged on as', self.user)
 
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message):
+        # only reply in specific channels
+        if message.channel.id not in self.ALLOWED_CHANNELS:
+            return
+
         # don't respond to ourselves
         if message.author == self.user:
             return
