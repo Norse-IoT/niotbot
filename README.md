cdd8debe421f218389dcc9eaf06b03b49db7d6b7
Zack Sargent <zack@zack.fyi>
2024-09-01 14:39:00 -0400

    basic discord bot

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 ```
+
+## join link
+
+<https://discordapp.com/oauth2/authorize?client_id=1279864612547723335&scope=bot&permissions=1689384584739904>
--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})'
+        )
+
+
 def main():
-    print("started!")
+    print("Starting...")
+    client.run(TOKEN)
 
 if __name__ == "__main__":
     main()
