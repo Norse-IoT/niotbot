0d0ee5af060d0d4f09db41767a61e24448c189f8
Zack Sargent <zack@zack.fyi>
2024-09-01 15:48:05 -0400

    create thread on message

--- niotbot.py
+++ niotbot.py
@@ -1,24 +1,49 @@
+import datetime
 import discord
+from discord import *
 
-class NIoTBot(discord.Client):
+
+class NIoTBot(Client):
     """ Norse IoT Bot; currently handles social media"""
+
+    APPROVERS_ROLE = "Social Media Approver"
 
     # this needs to be manually updated if you want to add more channels
     ALLOWED_CHANNELS = [
-        1279865902241808535
+        "social-media"
     ]
 
     async def on_ready(self):
         print('Logged on as', self.user)
 
-    async def on_message(self, message: discord.Message):
+    async def should_reject(self, message: Message) -> bool:
+        """All rejection and error handling logic"""
         # only reply in specific channels
-        if message.channel.id not in self.ALLOWED_CHANNELS:
-            return
+        if message.channel.name not in self.ALLOWED_CHANNELS:
+            return True
 
         # don't respond to ourselves
         if message.author == self.user:
+            return True
+
+        # add more guard statements here (if needed)
+
+        return False
+
+    async def on_message(self, message: Message):
+        if await self.should_reject(message):
             return
 
-        if message.content == 'ping':
-            await message.channel.send('pong')
+        current_time = datetime.datetime.now().isoformat()
+        thread = await message.channel.create_thread(
+            name=f"{message.author.display_name}'s submission",
+            type=ChannelType.private_thread,
+            message=message
+        )
+        await thread.send(f"Thanks for your submission {message.author.mention}.")
+        if not message.attachments:
+            await thread.send("No attachments were found. You must have attachments to post to social media.")
+            return
+        role: Role = utils.get(message.guild.roles, name=self.APPROVERS_ROLE)
+        assert role is not None
+        await thread.send(f"A {role.mention} will need to approve your submission before it is posted.")
